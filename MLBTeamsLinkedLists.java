fa0cf26324300a4b881c2a296b3384a9fb886cce
Justin Canose <50759336+jcanose81@users.noreply.github.com>
2019-06-25 11:41:55 -0400

    Add files via upload
    Unable to get .addLast function working. It should put that line of code last when executed, but using JDoodle, it isn't placing that line last.

---
+++ MLBTeamsLinkedLists.java
@@ -0,0 +1,15 @@
+﻿import java.util.LinkedList;
+
+public class MyClass {
+    public static void main(String args[]) {
+        LinkedList mlbAlEastTeams = new LinkedList();
+        
+        //Add Items
+        mlbAlEastTeams.addLast("Baltimore Orioles");
+        mlbAlEastTeams.addFirst("Boston Red Sox");
+        mlbAlEastTeams.add("New York Yankees");
+        mlbAlEastTeams.add("Tampa Bay Rays");
+        mlbAlEastTeams.add("Toronto Blue Jays");
+        System.out.println(mlbAlEastTeams);   
+    }
+}
